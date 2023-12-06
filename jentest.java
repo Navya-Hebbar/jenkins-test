26b4e0086daf077bbba2ee0a959e52e56cbe5623
Navya-Hebbar <navyahebbar032005@gmail.com>
2023-12-05 16:47:16 -0800

    Adding a java file to test

---
+++ jentest.java
@@ -0,0 +1,12 @@
+public class jentest
+{
+public static void main(String [] args)
+{
+int i;
+System.out.println("Hello Good Evening");
+System.out.println("Welcome to Hello World");
+for(i in range(10))
+{
+System.out.println("Hello WOrld!!!")
+}
+}
